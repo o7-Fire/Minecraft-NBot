d966eb02eb378a450f9678a3b00947ecd7a35dbf
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-05 01:35:13 +0800

    fix fr

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ with open("config.txt", "r") as f:
 for i in range(int(configlist[7])):
     def p():
         path = os.path.abspath(__file__).replace(r"\run.py", "")
-        os.system(f'node {path}\\main\\main.js {configlist[0]} {configlist[1]} {configlist[2]} {configlist[3]} {configlist[4]} {configlist[5]}{str(i+1)} {configlist[6]}')
+        os.system(f'node {path}\\main\\main.js {configlist[0]} {configlist[1]} {configlist[2]} {configlist[3] + str(i+1)} {configlist[4]} {configlist[5]} {configlist[6]} {configlist[3]}')
     t = threading.Thread(target=p)
     t.start()
     time.sleep(6)
