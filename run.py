a172970d38dfc31bbcab0fe078f78f4b88b1b75c
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-01-05 00:18:08 +0800

    Update run.py

--- run.py
+++ run.py
@@ -6,12 +6,13 @@ configlist = []
 with open("config.txt", "r") as f:
     configs = f.read().split("\n")
     for config in configs:
-        config = config.split("//")[0].split(": ")[1]
+        config = config.split(" //")[0].split(": ")[1]
         configlist.append(config)
         
-for i in range(5):
+for i in range(int(configlist[7])):
     def p():
-        os.system(f'node /main/main.js {configlist[0]} {configlist[1]} {configlist[2]} {configlist[3]} {configlist[4]} {configlist[5]}{str(i+1)} {configlist[6]}')
+        path = os.path.abspath(__file__).replace(r"\run.py", "")
+        os.system(f'node {path}\\main\\main.js {configlist[0]} {configlist[1]} {configlist[2]} {configlist[3]} {configlist[4]} {configlist[5]}{str(i+1)} {configlist[6]}')
     t = threading.Thread(target=p)
     t.start()
     time.sleep(6)
